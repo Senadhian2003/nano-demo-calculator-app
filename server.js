0ada091f2c4583ef9138349d722fc72d04229297
Senadhian2003 <senadhian@gmail.com>
2023-08-22 21:26:21 +0530

    First Commit

--- server.js
+++ server.js
@@ -1,25 +1,40 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use( bodyParser.json() );       
+app.use(bodyParser.urlencoded({extended: true})); 
 const PORT = process.env.PORT || 8080;
 
 const baseUrl = '/calculator'
 
-app.use(express.json());
+// app.use(express.json());
 
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    
+ 
+    return res.send('Hello World');
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
-});
+
+    const num = req.body;
+    const num1 = num.first;
+    const num2 = num.second;
+    console.log(num)
+
+    res.json({ "result": num1+num2 });
+}); 
 
 
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+
+    const num = req.body;
+    const num1 = num.first;
+    const num2 = num.second;
+    res.json({ "result": num1-num2 });
 });
 
 app.use(baseUrl, baseRouter);
